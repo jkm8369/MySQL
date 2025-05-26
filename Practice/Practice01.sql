bc71718c35f53661ece0e2193dc52008d9a9b0b3
jkm8369 <jkm8369@naver.com>
2025-05-26 17:51:16 +0900

    2025-05-26

--- Practice/Practice01.sql
+++ Practice/Practice01.sql
@@ -73,7 +73,7 @@ order by salary desc
 */
 select first_name 이름,
 	   salary 월급,
-       hire_date 입사일,
+       date_format(hire_date, '%Y-%m') 입사일,
        department_id 부서번호
 from employees
 where department_id in(10, 90, 100)
